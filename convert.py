1982b7f2e90ef44a264434ff871e7dafcb4b69ac
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-10-10 23:50:43 +0200

    normalize data, add missing people

--- convert.py
+++ convert.py
@@ -1,14 +1,22 @@
 import json
 from itertools import groupby
 import yaml
+import re
 x = json.load(open('fetch.json'))
 
 sections = [];
+
+def reverse(v):
+    v.reverse()
+    return v
 
 for (club, persons) in groupby(sorted(x, key=lambda x:x['club_short']), lambda x: x['club_short']):
     name = club.replace('.', '')
     with open('_data/{}.yml'.format(name), 'w') as fp:
         y = list(persons)
+        for v in y:
+            v['twitter_links'] = [re.sub('\?.+?$', '', x.replace('https://twitter.com/', '')) for x in v.get('twitter_links', [])]
+            v['phone_numbers'] = [re.sub('[\(\) ]', '', x) for x in v.get('phone_numbers', [])]
         content = {"websites": y}
         sections.append({
             "id": name, 
@@ -22,4 +30,4 @@ with open('_data/sections.yml', 'w+') as fp:
     fp.write(yaml.dump(sorted(sections, reverse=True, key=lambda x: x['size'])))
 
 with open('fetch.json', 'w+') as fp:
-    fp.write(json.dumps(sorted(x, reverse=True, key=lambda x: x['name'].split(" ")[-1]), indent=4, sort_keys=True))
+    fp.write(json.dumps(sorted(x, reverse=True, key=lambda x: " ".join(reverse(x['name'].split(" ")))), indent=4, sort_keys=True))
